ec8fa1873d2ccef7b05e42155199eeee693b6a6e
Alex <alexibb1312@yandex.ru>
2022-04-05 21:33:25 +0300

    Add manage log py_script

--- system l_a cheker/ck.py
+++ system l_a cheker/ck.py
@@ -1,23 +1,47 @@
 import sys
 import time
 import os
+import shutil
+#import subprocess
+#import re
 
-average = sys.argv[1]
-limit   = sys.argv[2]
+#from sh arguments
+load_average = sys.argv[1]
+limit = sys.argv[2]
 
-
-named_tuple = time.localtime() 
-time_string = time.strftime("%d/%m/%Y, в %H:%M:%S", named_tuple)
+#local date and time
+time_string = time.strftime("%d/%m/%Y, в %H:%M:%S", time.localtime())
 
 #code to check correct work
-if float(average) >= float(limit):
+if float(load_average) >= float(limit):
     ind = "ALERT"
 else:
     ind = "norm"
-print("load average is: "+str(average)+" index: "+ind+" time: "+ str(time_string) )
+print("load average is: "+str(load_average)+" index: "+ind+" time: " + str(time_string))
 #/code to check correct work
 
 #work with logging
+limitsize = 5
+logscount = 5
+if (os.path.isfile("average_log") == True):   #if logfile exist
+    log_size = os.stat("average_log").st_size / 1024  #get logfile size in kilobites
+    print("logsize: "+str(log_size))
+    print("limitsize: "+str(limitsize))
+    if (log_size >= limitsize):
+       print("logsize >= limitsize")
+       for currentFileNum in range(logscount, 1, -1):
+           src = "average_log" + str(currentFileNum-1)
+           dst = "average_log" + str(currentFileNum)
+           if(os.path.isfile(src) == True):
+               shutil.copyfile(src,dst)
+       shutil.copyfile("average_log", "average_log"+"1")
+       log_file = open("average_log", 'w')
+       log_file.close()
+
+
+
+
+
 
 
 #work with logging
